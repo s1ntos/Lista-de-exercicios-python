6568dbac8df62d85818e776c33682c9748779268
João <109487647+s1ntos@users.noreply.github.com>
2025-09-05 10:55:09 -0300

    Delete av2.py

--- av2.py
+++
@@ -1,7 +0,0 @@
-numero1 = int(input("Digite o primeiro número: "))
-numero2 = int(input("Digite o segundo número: "))
-
-if numero1 > numero2:
-    print("O primeiro número é maior do que o segundo")
-else:
-    print("O segundo número é maior do que o primeiro")
