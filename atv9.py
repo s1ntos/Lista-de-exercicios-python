5e327035e2195194e9b30759b2a4f6519e834b9a
João <109487647+s1ntos@users.noreply.github.com>
2025-09-05 10:55:03 -0300

    Delete atv9.py

--- atv9.py
+++
@@ -1,5 +0,0 @@
-numero = int(input("Digite um número inteiro positivo: "))
-while numero <= 0: 
-    numero = int(input("Número inválido! Digite um número inteiro positivo: "))
-digitos = len(str(numero))  
-print(f"O número {numero} tem {digitos} dígitos")
