6b276082cca2592c22970b9cb350e9d7c5f380e2
João <109487647+s1ntos@users.noreply.github.com>
2025-09-05 10:54:36 -0300

    Delete atv5.py

--- atv5.py
+++
@@ -1,9 +0,0 @@
-nome = input("Digite seu nome: ")
-senha = input("Digite sua senha: ") 
-senha1 = input("Digite novamente sua senha: ")
-
-while senha1 != senha:
-    print("Senha incorreta. Tente novamente.")
-    senha1 = input("Digite novamente sua senha: ")  
-
-print("Seja bem-vindo", nome)
