f2f76399cddd947d10c97beeae7fdf307c06c26e
João <109487647+s1ntos@users.noreply.github.com>
2025-09-18 19:19:15 -0300

    Update atv1.py

--- lista_01/atv1.py
+++ lista_01/atv1.py
@@ -3,4 +3,17 @@ num = int(input("Digite um número: "))
 if num % 2 == 0:
     print("O número é par")
 else:
+
     print("O número é ímpar")
+
+
+
+
+def main():
+    """Ecoa os argumentos de entrada para a saída padrão."""
+    phrase = shlex.join(sys.argv)
+    echo(phrase)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main()) 
